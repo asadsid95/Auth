71a9d1e245e3697ed3e8b23a37f06f3ba6150bb0
Asad Siddiqui <asadsid95@gmail.com>
2019-12-01 02:44:07 -0500

    BE: created GET request

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express")
 const app = express()
-const dotenv = require("dotenv")
+const dotenv = require("dotenv").config()
 //app.use(express.static("public"))
 const mongoose = require("mongoose")
 
 //Import routes
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
-
+const econDataRoute = require('./routes/econData')
 //dotenv.config()
 
 //Connect to DB
-mongoose.connect("mongodb://localhost:27017/hello",
+mongoose.connect(process.env.MONGODB,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true
@@ -25,5 +25,6 @@ app.use(express.json())
 //Route middleware
 app.use('/api/user', authRoute)
 app.use("/api/posts", postRoute)
+app.use(econDataRoute)
 
 app.listen(3000, () => console.log("Node.JS Server listening to 3000 using Express framework"))
---
+++ routes/econData.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+// GET request to obtain data
+
+// UPDATE:
+// Currently CAN'T get the raw data
+router.get("/ind-econ.json", async (req, res) => {
+  try {
+    res.status(200).send("code obtained!");
+  } catch (err) {
+    res.status(400).send("error getting list");
+  }
+});
+
+module.exports = router;
