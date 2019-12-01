9c7fc3c0f042f289801476b224a1bc63bf2f5306
Asad Siddiqui <asadsid95@gmail.com>
2019-12-01 02:27:17 -0500

    Revert " BE: GET request to obtain data"
    This reverts commit c366d283c2362049e7974c16805055f4d8063ac6.

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
 //app.use(express.static("public"))
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 //Import routes
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const econDataRoute = require("./routes/econData")
+const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 
 //dotenv.config()
 
 //Connect to DB
-mongoose.connect("mongodb://localhost:27017/hello", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
-mongoose.connection.on("connected", () => {
-  console.log("Connected MongoDB using Mongoose.");
-});
+mongoose.connect("mongodb://localhost:27017/hello",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.on("connected", () => {console.log("hello")})
 
 //Middleware
-app.use(express.json());
+app.use(express.json())
 
 //Route middleware
-app.use("/api/user", authRoute);
-app.use("/api/posts", postRoute);
-app.use(econDataRoute);
+app.use('/api/user', authRoute)
+app.use("/api/posts", postRoute)
 
-app.listen(3000, () => console.log("Node.JS Server listening to 3000 using Express framework"));
+app.listen(3000, () => console.log("Node.JS Server listening to 3000 using Express framework"))
